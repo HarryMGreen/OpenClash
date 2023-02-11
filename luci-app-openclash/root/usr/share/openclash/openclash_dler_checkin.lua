03c02515fc62b0bc608eb31b0768a68ddb68caa0
HarryMGreen <harrygreensmile@gmail.com>
2023-02-12 02:20:55 +0800

    remove annoying log

--- luci-app-openclash/root/usr/share/openclash/openclash_dler_checkin.lua
+++ luci-app-openclash/root/usr/share/openclash/openclash_dler_checkin.lua
@@ -1,15 +1,1 @@
 #!/usr/bin/lua
-
-require "nixio"
-require "luci.util"
-require "luci.sys"
-local uci = require("luci.model.uci").cursor()
-local fs = require "luci.openclash"
-local json = require "luci.jsonc"
-
-local function dler_checkin()
-	luci.sys.exec(string.format('echo "%s Dler Cloud Checkin Failed! Please Check And Try Again..." >> /tmp/openclash.log', os.date("%Y-%m-%d %H:%M:%S")))
-	os.exit(0)
-end
-
-dler_checkin()
