0eae6ed1ddda038603e470b994ea001103ee685e
HarryMGreen <harrygreensmile@gmail.com>
2023-11-24 00:08:09 +0800

    remove version check

--- luci-app-openclash/root/usr/share/openclash/clash_version.sh
+++ luci-app-openclash/root/usr/share/openclash/clash_version.sh
@@ -1,34 +1,38 @@
 #!/bin/bash
 
-set_lock() {
-   exec 884>"/tmp/lock/openclash_clash_version.lock" 2>/dev/null
-   flock -x 884 2>/dev/null
-}
+echo "0.0.0" > /tmp/clash_last_version
+echo "0.0.0" >> /tmp/clash_last_version
+echo "0.0.0" >> /tmp/clash_last_version
 
-del_lock() {
-   flock -u 884 2>/dev/null
-   rm -rf "/tmp/lock/openclash_clash_version.lock"
-}
-
-TIME=$(date "+%Y-%m-%d-%H")
-CHTIME=$(date "+%Y-%m-%d-%H" -r "/tmp/clash_last_version")
-LAST_OPVER="/tmp/clash_last_version"
-RELEASE_BRANCH=$(uci -q get openclash.config.release_branch || echo "master")
-github_address_mod=$(uci -q get openclash.config.github_address_mod || echo 0)
-LOG_FILE="/tmp/openclash.log"
-set_lock
-
-if [ "$TIME" != "$CHTIME" ]; then
-   curl -SsL -m 10 https://raw.githubusercontent.com/vernesong/OpenClash/"$RELEASE_BRANCH"/core_version -o $LAST_OPVER 2>&1 | awk -v time="$(date "+%Y-%m-%d %H:%M:%S")" -v file="$LAST_OPVER" '{print time "【" file "】Download Failed:【"$0"】"}' >> "$LOG_FILE"
-   
-   if [ "${PIPESTATUS[0]}" -ne 0 ] || [ -n "$(cat $LAST_OPVER |grep '<html>')" ]; then
-      curl_status=${PIPESTATUS[0]}
-   else
-      curl_status=0
-   fi
-   
-   if [ "$curl_status" -ne 0 ] ; then
-      rm -rf $LAST_OPVER
-   fi
-fi
-del_lock
+#set_lock() {
+#   exec 884>"/tmp/lock/openclash_clash_version.lock" 2>/dev/null
+#   flock -x 884 2>/dev/null
+#}
+#
+#del_lock() {
+#   flock -u 884 2>/dev/null
+#   rm -rf "/tmp/lock/openclash_clash_version.lock"
+#}
+#
+#TIME=$(date "+%Y-%m-%d-%H")
+#CHTIME=$(date "+%Y-%m-%d-%H" -r "/tmp/clash_last_version")
+#LAST_OPVER="/tmp/clash_last_version"
+#RELEASE_BRANCH=$(uci -q get openclash.config.release_branch || echo "master")
+#github_address_mod=$(uci -q get openclash.config.github_address_mod || echo 0)
+#LOG_FILE="/tmp/openclash.log"
+#set_lock
+#
+#if [ "$TIME" != "$CHTIME" ]; then
+#   curl -SsL -m 10 https://raw.githubusercontent.com/vernesong/OpenClash/"$RELEASE_BRANCH"/core_version -o $LAST_OPVER 2>&1 | awk -v time="$(date "+%Y-%m-%d %H:%M:%S")" -v file="$LAST_OPVER" '{print time "【" file "】Download Failed:【"$0"】"}' >> "$LOG_FILE"
+#   
+#   if [ "${PIPESTATUS[0]}" -ne 0 ] || [ -n "$(cat $LAST_OPVER |grep '<html>')" ]; then
+#      curl_status=${PIPESTATUS[0]}
+#   else
+#      curl_status=0
+#   fi
+#   
+#   if [ "$curl_status" -ne 0 ] ; then
+#      rm -rf $LAST_OPVER
+#   fi
+#fi
+#del_lock
--- luci-app-openclash/root/usr/share/openclash/openclash_version.sh
+++ luci-app-openclash/root/usr/share/openclash/openclash_version.sh
@@ -1,36 +1,40 @@
 #!/bin/bash
-TIME=$(date "+%Y-%m-%d-%H")
-CHTIME=$(date "+%Y-%m-%d-%H" -r "/tmp/openclash_last_version")
-LAST_OPVER="/tmp/openclash_last_version"
-RELEASE_BRANCH=$(uci -q get openclash.config.release_branch || echo "master")
-OP_CV=$(rm -f /var/lock/opkg.lock && opkg status luci-app-openclash 2>/dev/null |grep 'Version' |awk -F '-' '{print $1}' |awk -F 'Version: ' '{print $2}' |awk -F '.' '{print $2$3}' 2>/dev/null)
-OP_LV=$(sed -n 1p $LAST_OPVER 2>/dev/null |awk -F '-' '{print $1}' |awk -F 'v' '{print $2}' |awk -F '.' '{print $2$3}' 2>/dev/null)
-github_address_mod=$(uci -q get openclash.config.github_address_mod || echo 0)
-LOG_FILE="/tmp/openclash.log"
 
-if [ "$TIME" != "$CHTIME" ]; then
-   curl -SsL --connect-timeout 10 -m 30 --speed-time 15 --speed-limit 1 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/"$RELEASE_BRANCH"/version -o $LAST_OPVER 2>&1 | awk -v time="$(date "+%Y-%m-%d %H:%M:%S")" -v file="$LAST_OPVER" '{print time "【" file "】Download Failed:【"$0"】"}' >> "$LOG_FILE"
+echo "0.0.0" > /tmp/openclash_last_version
 
-   if [ "${PIPESTATUS[0]}" -ne 0 ] || [ -n "$(cat $LAST_OPVER |grep '<html>')" ]; then
-      curl_status=${PIPESTATUS[0]}
-   else
-      curl_status=0
-   fi
-
-   if [ "$curl_status" -eq 0 ] && [ -z "$(cat $LAST_OPVER |grep '<html>')" ]; then
-   	OP_LV=$(sed -n 1p $LAST_OPVER 2>/dev/null |awk -F '-' '{print $1}' |awk -F 'v' '{print $2}' |awk -F '.' '{print $2$3}' 2>/dev/null)
-      if [ "$(expr "$OP_CV" \>= "$OP_LV")" = "1" ]; then
-         sed -i '/^https:/,$d' $LAST_OPVER
-      elif [ "$(expr "$OP_LV" \> "$OP_CV")" = "1" ] && [ -n "$OP_LV" ]; then
-         exit 2
-      else
-         exit 0
-      fi
-   else
-      rm -rf "$LAST_OPVER"
-   fi
-elif [ "$(expr "$OP_LV" \> "$OP_CV")" = "1" ] && [ -n "$OP_LV" ]; then
-   exit 2
-else
-   exit 0
-fi 2>/dev/null
+#TIME=$(date "+%Y-%m-%d-%H")
+#CHTIME=$(date "+%Y-%m-%d-%H" -r "/tmp/openclash_last_version")
+#LAST_OPVER="/tmp/openclash_last_version"
+#RELEASE_BRANCH=$(uci -q get openclash.config.release_branch || echo "master")
+#OP_CV=$(rm -f /var/lock/opkg.lock && opkg status luci-app-openclash 2>/dev/null |grep 'Version' |awk -F '-' '{print $1}' |awk -F 'Version: ' '{print $2}' |awk -F '.' '{print $2$3}' 2>/dev/null)
+#OP_LV=$(sed -n 1p $LAST_OPVER 2>/dev/null |awk -F '-' '{print $1}' |awk -F 'v' '{print $2}' |awk -F '.' '{print $2$3}' 2>/dev/null)
+#github_address_mod=$(uci -q get openclash.config.github_address_mod || echo 0)
+#LOG_FILE="/tmp/openclash.log"
+#
+#if [ "$TIME" != "$CHTIME" ]; then
+#   curl -SsL --connect-timeout 10 -m 30 --speed-time 15 --speed-limit 1 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/"$RELEASE_BRANCH"/version -o $LAST_OPVER 2>&1 | awk -v time="$(date "+%Y-%m-%d %H:%M:%S")" -v file="$LAST_OPVER" '{print time "【" file "】Download Failed:【"$0"】"}' >> "$LOG_FILE"
+#
+#   if [ "${PIPESTATUS[0]}" -ne 0 ] || [ -n "$(cat $LAST_OPVER |grep '<html>')" ]; then
+#      curl_status=${PIPESTATUS[0]}
+#   else
+#      curl_status=0
+#   fi
+#
+#   if [ "$curl_status" -eq 0 ] && [ -z "$(cat $LAST_OPVER |grep '<html>')" ]; then
+#   	OP_LV=$(sed -n 1p $LAST_OPVER 2>/dev/null |awk -F '-' '{print $1}' |awk -F 'v' '{print $2}' |awk -F '.' '{print $2$3}' 2>/dev/null)
+#      if [ "$(expr "$OP_CV" \>= "$OP_LV")" = "1" ]; then
+#         sed -i '/^https:/,$d' $LAST_OPVER
+#      elif [ "$(expr "$OP_LV" \> "$OP_CV")" = "1" ] && [ -n "$OP_LV" ]; then
+#         exit 2
+#      else
+#         exit 0
+#      fi
+#   else
+#      rm -rf "$LAST_OPVER"
+#   fi
+#elif [ "$(expr "$OP_LV" \> "$OP_CV")" = "1" ] && [ -n "$OP_LV" ]; then
+#   exit 2
+#else
+#   exit 0
+#fi 2>/dev/null
+#
